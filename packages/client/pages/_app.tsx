5713f8a46b268bb6d78e30fd405f41489d78c312
Kevin Ruhl <kevin.ruhl92@gmail.com>
2022-11-01 01:41:03 -0300

    pass color scheme to them config init

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -51,7 +51,7 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
         toggleColorScheme={toggleColorScheme}
       >
         <MantineProvider
-          theme={{ ...themeConfig() }}
+          theme={{ ...themeConfig(colorScheme) }}
           withNormalizeCSS
           withGlobalStyles
         >
