a65714c065244e342e74886570e3250eccd8dbfd
Kevin Ruhl <kevin.ruhl92@gmail.com>
2023-06-26 22:59:00 -0300

    ignores dist folder

--- packages/server/dist/static/index-cf8d3806.esm.14d11b2f.js
+++
@@ -1,1 +0,0 @@
-import{a as t,D as m}from"./index-989e9e04.esm.8e0138c8.js";import"./sanity.ed39ef92.js";import"./index.4bcbe3f5.js";export{t as DocumentPaneProvider,m as default};
--- packages/server/dist/static/index-cf8d3806.esm.9e011b3e.js
+++
@@ -1,1 +0,0 @@
-import{a as t,D as m}from"./index-989e9e04.esm.d539a05a.js";import"./sanity.35d959cb.js";import"./index.4b22f47e.js";export{t as DocumentPaneProvider,m as default};
