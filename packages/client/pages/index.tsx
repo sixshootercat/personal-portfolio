ca28f2cdc800bec03b2146d7fb9bda7c20cfbce4
Kevin Ruhl <kevin.ruhl92@gmail.com>
2023-09-11 03:23:17 -0300

    chore: installs zod and adds prop validation to sanity fetched data; adds image blur data

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,24 +1,26 @@
-import { Home as HomeContent } from '@/components/home';
-import { GetStaticProps } from 'next';
-import { client } from '@/sanity/sanity-client';
+import { Home as HomeContent } from "@/components/home";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { client } from "@/sanity/sanity-client";
+import Head from "next/head";
+import {
+  TECHNOLOGIES_QUERY,
+  ABOUT_ME_QUERY,
+  technologiesSchema,
+} from "@/sanity/queries";
+import type { Technologies as ITechnologies } from "@/sanity/queries";
+import { AboutMe, ContactMe, Technologies } from "@/components/home/components";
 
-import Head from 'next/head';
-import { TECHNOLOGIES_QUERY, ABOUT_ME_QUERY } from '@/sanity/queries';
-import { AboutMe, ContactMe, Technologies } from '@/components/home/components';
-
-type HomeProps = {
-  technologies: any;
-  aboutMe: any;
-};
-
-const Home = ({ technologies, aboutMe }: HomeProps) => {
+const Home = ({
+  technologies,
+  aboutMe,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className='mt-24'>
+    <div className="mt-24">
       <Head>
         <title>Home</title>
       </Head>
       <HomeContent>
-        <div className='mb-8 flex items-center justify-center flex-col mt-10 md:px-20 max-w-[1440px] w-full m-auto'>
+        <div className="mb-8 flex items-center justify-center flex-col mt-10 md:px-20 max-w-[1440px] w-full m-auto">
           <AboutMe sanityData={aboutMe} />
           <Technologies sanityData={technologies} />
           <ContactMe />
@@ -28,14 +30,15 @@ const Home = ({ technologies, aboutMe }: HomeProps) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const getTechnologies = async () => await client.fetch(TECHNOLOGIES_QUERY);
-  const getAboutMe = async () => await client.fetch(ABOUT_ME_QUERY);
+export const getStaticProps: GetStaticProps<{
+  technologies: ITechnologies;
+  aboutMe: any; //TODO: improve typing (sanity image and protable text)
+}> = async () => {
+  const technologies = await client.fetch(TECHNOLOGIES_QUERY).then((res) => {
+    return technologiesSchema.parse(res);
+  });
 
-  const [technologies, aboutMe] = await Promise.all([
-    getTechnologies(),
-    getAboutMe(),
-  ]);
+  const aboutMe = await client.fetch(ABOUT_ME_QUERY);
 
   console.log({ aboutMe });
 
--- packages/client/src/sanity/queries.ts
+++ packages/client/src/sanity/queries.ts
@@ -1,4 +1,20 @@
-import { groq } from 'next-sanity';
+import { groq } from "next-sanity";
+import { z } from "zod";
 
-export const TECHNOLOGIES_QUERY = groq`*[_type == 'technologies']`;
+export const technologiesSchema = z.array(
+  z.object({
+    _id: z.string(),
+    name: z.string().nullable(),
+    url: z.string().optional(),
+  })
+);
+
+export type Technologies = z.infer<typeof technologiesSchema>;
+
+export const TECHNOLOGIES_QUERY = groq`*[_type == 'technologies']{
+  _id,
+  name,
+  url
+}`;
+
 export const ABOUT_ME_QUERY = groq`*[_type == 'aboutMe']`;
